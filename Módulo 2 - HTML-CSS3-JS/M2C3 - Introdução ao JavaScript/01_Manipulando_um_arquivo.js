855cceba52578c789312033279ec0dda287bddc9
jefersonmatsui <jefersonmatsui@gmail.com>
2022-07-18 10:50:50 -0300

    Introdução

---
+++ Módulo 2 - HTML-CSS3-JS/M2C3 - Introdução ao JavaScript/01_Manipulando_um_arquivo.js
@@ -0,0 +1,24 @@
+// var y = 0; (Para fazer comentário de uma linha usa-se a tag '//'
+var x = 1;
+
+// para comentário de vairas linhas usa-se começa com a tag /* e termina com */ 
+/* var y = 0;
+var x = 1; */
+var z = 2
+
+// ctrl + / , seleciona todas como comentário
+var y = 0;
+var x = 1;
+var z = 2;
+
+//Exemplo 1:
+var preco = 2;
+var desconto = 0.2;
+preco = preco - desconto;
+
+//Exemplo 2:
+var desconto = 0.2;
+// Constantes são declaradas com letras maiuscúlas
+const PRECO = 2;
+var total = PRECO - desconto; 
+
---
+++ Módulo 2 - HTML-CSS3-JS/M2C3 - Introdução ao JavaScript/02_Funcoes.js
@@ -0,0 +1,7 @@
+// parênteses '()' serve para declaração de atributos, parâmetros
+
+function soma(a, b) {
+  return a + b;
+}
+
+soma(3, 5); // vai retornar 8
