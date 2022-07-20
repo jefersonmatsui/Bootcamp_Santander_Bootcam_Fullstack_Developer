8414e1bcfb0555c6b482a38213737b1c3191c29f
jefersonmatsui <jefersonmatsui@gmail.com>
2022-07-20 10:56:47 -0300

    Atribuindo Valores

---
+++ Módulo 2 - HTML-CSS3-JS/M2C5 - Variáveis e Tipos com JavaScript/103.var_let_const.js
@@ -0,0 +1,21 @@
+/*
+var
+escopo === global ou local
+redeclarar === sim
+reatribuir === sim
+hoisting === sim
+
+const
+escopo === bloco
+redeclarar === não
+reatribuir === não
+hoisting === não
+
+let
+escopo === bloco
+redeclarar === não
+reatribuir === sim
+hoisting === não
+*/
+
+// hoisting - pode declarar depois
