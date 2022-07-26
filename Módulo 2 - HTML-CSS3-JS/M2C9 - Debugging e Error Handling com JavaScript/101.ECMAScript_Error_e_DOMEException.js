2900b8a9e59cf4e9cff1e4fa5c06b7c5df0c2bb9
jefersonmatsui <jefersonmatsui@gmail.com>
2022-07-26 09:32:51 -0300

    Tipos de erros

---
+++ Módulo 2 - HTML-CSS3-JS/M2C9 - Debugging e Error Handling com JavaScript/101.ECMAScript_Error_e_DOMEException.js
@@ -0,0 +1,17 @@
+// 1. ECMAScript Error
+
+/* 
+Erros que ocorrem em tempo de execução.
+Composto por:
+- Mensagem
+- Nome
+- Linha
+- Call Stack
+*/
+
+
+// 2. DOMException
+
+/*
+Erros relacionados ao Document Object Model (DOM)
+*/
