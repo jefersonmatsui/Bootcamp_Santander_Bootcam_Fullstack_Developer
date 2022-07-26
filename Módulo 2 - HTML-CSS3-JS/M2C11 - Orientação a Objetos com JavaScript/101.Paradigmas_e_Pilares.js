baf07d7dfa8ec5534b9c417e33f342dd69cf8e09
jefersonmatsui <jefersonmatsui@gmail.com>
2022-07-26 16:14:53 -0300

    Compreendendo Orientação a objetos

---
+++ Módulo 2 - HTML-CSS3-JS/M2C11 - Orientação a Objetos com JavaScript/101.Paradigmas_e_Pilares.js
@@ -0,0 +1,46 @@
+// 1. Paradigmas
+
+/*
+Paradigms
+  - Imperative;
+    - Procedural;
+    - Object Oriented;
+    - Parallel Processing;
+  
+  - Declarative;
+    - Logic;
+    - Functional;
+      - Dataflow;
+    - Database
+
+
+- Os programas são 'objetos' que possuem um série de propriedadees
+Pilares:
+  - Herança
+  - Polimorfismo
+  - Encapsulamento
+  - Abstração
+*/
+
+// 2. Pilares
+
+// 2.1. Abstração
+/*
+"Processo mental que consiste em isaolar um aspecto determinado de um estado de coisas relativamente complexo,
+a fim de simplificar a asua avaliação, classificação ou para permitir a comunicação do mesmo."
+*/
+
+// 2.2. Herança
+/*
+O objeto filho herda propeirdades e métodos do objeto pai
+*/
+
+// 2.3. Encapsulamento
+/*
+Cada classe tem propriedades e métodos independentes do restante do código
+*/
+
+// 2.4. Polimorfismo
+/*
+Objetos podem herdar a mesma classe pai, mas se comportarem de forma diferente quando invocamos seus métodos
+*/
